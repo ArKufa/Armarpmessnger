2ee8d60cd6354ba3a5d3937b7e30acbb593ccc0e
Mikiras <151003493+ArKufa@users.noreply.github.com>
2025-10-30 01:51:38 +0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+# Для Render.com
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Важно: исправляем URL для SQLAlchemy
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
