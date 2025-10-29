512bc9f324bd154f3415c089202ab883dd6fd43f
Mikiras <151003493+ArKufa@users.noreply.github.com>
2025-10-30 01:55:37 +0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,11 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Для Render.com
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://arma_user:QRs0QZwsuSQjW2LX3pHfvMttLlYiuAbb@dpg-d418obili9vc739h7t8g-a/arma_messenger")
 
 # Важно: исправляем URL для SQLAlchemy
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+if DATABASE_URL and DATABASE_URL.startswith("postgresql://arma_user:QRs0QZwsuSQjW2LX3pHfvMttLlYiuAbb@dpg-d418obili9vc739h7t8g-a/arma_messenger"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://arma_user:QRs0QZwsuSQjW2LX3pHfvMttLlYiuAbb@dpg-d418obili9vc739h7t8g-a/arma_messenger", "postgresql://arma_user:QRs0QZwsuSQjW2LX3pHfvMttLlYiuAbb@dpg-d418obili9vc739h7t8g-a/arma_messenger", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
